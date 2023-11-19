ca869b5f8a87138df9a95d669e5d8177f9707a37
Leo Booth <leoboothtx@gmail.com>
2023-11-18 23:36:45 -0600

    implemented test case 1: character list

--- test/swapi-tests.js
+++ test/swapi-tests.js
@@ -19,28 +19,59 @@ describe('Star Wars API Tests', function () {
     // Test Case 1: Retrieve a list of all Star Wars characters
     it  ('should retrieve a list of all Star Wars characters', 
     async function () {
+        // retrieving an initial result often takes over 5000ms
+        let timeoutInMs = 10000;
+        this.timeout(timeoutInMs);
         const response = await axios.get(`${swapiUrl}/people`);
-        assert.strictEqual(response.status, 200, 'Unexpected status code');
-        // TODO: Implement the remainder of this test case
+        
+        const charCount = response.data.count;
+        console.log(`character count: ${charCount}`);
+
+        let resultsPerFullPage = 10;
+        console.log(`results per page: ${resultsPerFullPage}`);
+
+        let maxPages = Math.floor(charCount / resultsPerFullPage) + 1;
+        console.log(`max pages: ${maxPages}`);
+
+        let currentPageNumber = 1;
+        let characterList = [];
+        let currentPageUrl = "";
+        do {
+            this.timeout(timeoutInMs * maxPages);
+            currentPageUrl = `${swapiUrl}/people/?page=${currentPageNumber}`
+            const response = await axios.get(currentPageUrl);
+            console.log("currentPageUrl: " + currentPageUrl);
+
+            characterList = characterList.concat(response.data.results);
+            currentPageNumber++;
+        } while (response.data.next !== null && currentPageNumber <= maxPages);
+
+        console.log(`characterList length: ${characterList.length}`);
+        console.log("the character list contains the following characters: " );
+        for (let character in characterList) {
+            console.log(characterList[character].name);
+        }
+
+        assert.equal(charCount, characterList.length);
     });
 
-    // Test Case 2: Retrieve details for a specific Star Wars character
-    it('should retrieve details for a specific Star Wars character',
-    async function () {
-        const characterName = "Luke Skywalker";
-        // TODO: Implement the remainder of this test case
-    });
+    // // Test Case 2: Retrieve details for a specific Star Wars character
+    // it('should retrieve details for a specific Star Wars character',
+    // async function () {
+    //     const characterName = "Luke Skywalker";
+    //     // TODO: Implement the remainder of this test case
+    // });
         
-    // Test Case 3: Retrieve a list of all Star Wars films
-    it('should retrieve a list of all Star Wars films', 
-    async function () {
-        // TODO: Implement the test case
-    });
+    // // Test Case 3: Retrieve a list of all Star Wars films
+    // it('should retrieve a list of all Star Wars films', 
+    // async function () {
+    //     // TODO: Implement the test case
+    // });
         
-    // Test Case 4: Retrieve details for a specific Star Wars film
-    it('should retrieve details for a specific Star Wars film', 
-    async function () {
-        // TODO: Implement the test case
-    });
+    // // Test Case 4: Retrieve details for a specific Star Wars film
+    // it('should retrieve details for a specific Star Wars film', 
+    // async function () {
+    //     // TODO: Implement the test case
+    // });
 });
 
