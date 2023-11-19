c7b35eebac0356911d69102de2d79b7b67391914
Leo Booth <leoboothtx@gmail.com>
2023-11-18 21:49:37 -0600

    added test folder and starter code

---
+++ test/swapi-tests.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const assert = require('assert');
+// SWAPI endpoint
+const swapiUrl = "https://swapi.dev/api";
+
+// Run the tests
+// This is just a placeholder, candidates might need to customize this 
+// based on their project structure.
+// For example, if the script is in a "test" folder, candidates can use
+//`mocha test/*.js`.
+// Make sure Mocha is installed either globally or as a devDependency.
+// npm install -g mocha
+// or
+// npm install --save-dev mocha
+
+// Test Suite
+describe('Star Wars API Tests', function () {
+
+    // Test Case 1: Retrieve a list of all Star Wars characters
+    it  ('should retrieve a list of all Star Wars characters', 
+    async function () {
+        const response = await axios.get(`${swapiUrl}/people`);
+        assert.strictEqual(response.status, 200, 'Unexpected status code');
+        // TODO: Implement the remainder of this test case
+    });
+
+    // Test Case 2: Retrieve details for a specific Star Wars character
+    it('should retrieve details for a specific Star Wars character',
+    async function () {
+        const characterName = "Luke Skywalker";
+        // TODO: Implement the remainder of this test case
+    });
+        
+    // Test Case 3: Retrieve a list of all Star Wars films
+    it('should retrieve a list of all Star Wars films', 
+    async function () {
+        // TODO: Implement the test case
+    });
+        
+    // Test Case 4: Retrieve details for a specific Star Wars film
+    it('should retrieve details for a specific Star Wars film', 
+    async function () {
+        // TODO: Implement the test case
+    });
+});
+
